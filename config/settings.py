9696af756e9d080c2c315fe98ad2805b944c6469
ProgErnest <cuba7843@gmail.com>
2026-07-22 17:15:37 +0100

    Started the cinternationalisation in the setting.py

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -114,6 +116,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ("en",_("English")),
+    ("fr",_("Francais")),
+]
+
+LOCAL_PATH = BASE_DIR / "locale"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
